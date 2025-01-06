0cc210f68eff13916e554b2317aff7d00a620ca3
Mehdi <78029399+poly-mehdi@users.noreply.github.com>
2025-01-06 20:16:54 +0100

    Add Timeout waiting screen

--- client/frontend/app/session/[id]/page.tsx
+++ client/frontend/app/session/[id]/page.tsx
@@ -1,19 +1,39 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
-import { useSocketContext } from '@/app/providers/socket-provider'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import { socket } from '@/socket'
 import { LoadingSpinner } from '@/components/loading'
 
 function RoomPage() {
-  const pathname = usePathname()
-  const { isConnected, transport, clientId } = useSocketContext()
+  const router = useRouter()
+
+  useEffect(() => {
+    // const timeout = setTimeout(() => router.push('/session/contact'), 1000)
+    const timeout = setTimeout(
+      () => console.log('Timeout! Redirecting to /session/contact'),
+      1000
+    )
+
+    socket.once('advisor.connected', () => {
+      console.log('advisor.connected event received, clearing timeout')
+      clearTimeout(timeout)
+    })
+
+    return () => {
+      console.log('Cleaning up timeout')
+      clearTimeout(timeout)
+    }
+  }, [])
 
   return (
     <div className='flex min-h-svh flex-col items-center justify-center'>
-      <h1 className='text-2xl mb-10'>
-        An advisor will be with you shortly. Thank you for your patience.
-      </h1>
-      <LoadingSpinner size={100} />
+      <div className='flex flex-col items-center'>
+        <h1 className='text-2xl mb-10'>
+          An advisor will be with you shortly. Thank you for your patience.
+        </h1>
+        <LoadingSpinner size={100} />
+      </div>
     </div>
   )
 }
---
+++ client/frontend/app/session/contact/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { useEffect } from 'react'
+
+function ContactPage() {
+  return <div>ContactPage</div>
+}
+export default ContactPage
