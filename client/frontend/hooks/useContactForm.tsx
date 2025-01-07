bda6dce0ad3507ade27e873b99dee0b1af041763
Mehdi <78029399+poly-mehdi@users.noreply.github.com>
2025-01-07 12:49:28 +0100

    Add phone-input validation on client

--- client/frontend/hooks/useContactForm.tsx
+++ client/frontend/hooks/useContactForm.tsx
@@ -1,10 +1,14 @@
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { isValidPhoneNumber } from 'react-phone-number-input'
 
 // Validation schema
 export const formSchema = z.object({
-  phone: z.string(),
+  phone: z
+    .string()
+    .refine(isValidPhoneNumber, { message: 'Invalid phone number' })
+    .or(z.literal('')),
 })
 
 // Hook for form logic
