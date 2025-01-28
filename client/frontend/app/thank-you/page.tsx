0e1e2b392d9e2c3456ee69f5947071cf57dbfc1e
Mehdi <78029399+poly-mehdi@users.noreply.github.com>
2025-01-28 15:48:52 -0500

    Remove smiley icon

--- client/frontend/app/thank-you/page.tsx
+++ client/frontend/app/thank-you/page.tsx
@@ -1,9 +1,6 @@
-import { Smile } from 'lucide-react'
-
 function page() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center'>
-      <Smile className='mb-4' size={64} />
       <h1 className='text-5xl mb-4'>Thank You!</h1>
       <p className='text-xl'>
         Thank you for using our assistance service. We appreciate your visit.
@@ -12,3 +9,4 @@ function page() {
   )
 }
 export default page
+
