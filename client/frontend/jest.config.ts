67a671b6df42a3a5cd2e05ab8741f03777ef8e87
Mehdi <78029399+poly-mehdi@users.noreply.github.com>
2025-01-30 19:06:27 -0500

    Add hook test

--- client/frontend/jest.config.ts
+++ client/frontend/jest.config.ts
@@ -10,9 +10,15 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
+  coverageThreshold: {
+    global: {
+      lines: 80,
+    },
+  },
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
+
