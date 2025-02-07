acef1410f042e1bbe05a4d18a6882289b3364c3b
Mehdi <78029399+poly-mehdi@users.noreply.github.com>
2025-02-07 12:11:43 -0500

    Add lucide react loader

--- client/frontend/app/[locale]/session/[id]/page.tsx
+++ client/frontend/app/[locale]/session/[id]/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import { LoadingSpinner } from '@/components/loading';
 import { useRoomEvent } from '@/hooks/useRoomEvent';
 import { useRoomUrl } from '@/hooks/useRoomUrl';
 import '@whereby.com/browser-sdk/embed';
 import { useTranslations } from 'next-intl';
 import { WherebyProvider } from '@whereby.com/browser-sdk/react';
+import { Loader2 } from 'lucide-react';
 
 function RoomPage() {
   const t = useTranslations();
@@ -27,7 +27,7 @@ function RoomPage() {
     <div className='flex min-h-svh flex-col items-center justify-center'>
       <div className='flex flex-col items-center'>
         <h1 className='text-2xl mb-10'>{t('room.waiting-message')}</h1>
-        <LoadingSpinner size={100} data-testid='loading-spinner' />
+        <Loader2 className='animate-spin' size={100} />
       </div>
     </div>
   );
