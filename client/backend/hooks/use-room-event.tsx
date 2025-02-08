3ba2679d9b6d3116da2757718cc00fb5e3646866
Mehdi <78029399+poly-mehdi@users.noreply.github.com>
2025-02-08 10:18:12 -0500

    Add leave event from whereby room

--- client/backend/hooks/use-room-event.tsx
+++ client/backend/hooks/use-room-event.tsx
@@ -1,22 +1,30 @@
 import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useRef } from 'react';
 
+export type WherebyRef = HTMLDivElement & {
+  endMeeting: () => void;
+};
+
 export const useRoomEvent = (roomUrl: string | null) => {
   const whereByRef = useRef(null);
   const router = useRouter();
   const { id: sessionId } = useParams();
 
   useEffect(() => {
-    const elm = whereByRef.current as HTMLDivElement | null;
+    const elm = whereByRef.current as WherebyRef | null;
 
     if (elm) {
       const handleEvent = (event: any) => {
         if (event.type === 'meeting_end') {
           router.push(`/apps/session/session-evaluation/${sessionId}`);
         }
+        if (event.type === ' leave') {
+          elm.endMeeting();
+          router.push(`/apps/session/session-evaluation/${sessionId}`);
+        }
       };
 
-      const events = ['meeting_end'];
+      const events = ['leave', 'meeting_end'];
       events.forEach((event) => {
         elm.addEventListener(event, handleEvent);
       });
